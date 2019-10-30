bd3e0e87d94692ace38ccd02019705e996c49c34
Justin Smith <jssmith@gatech.edu>
2019-10-29 23:20:14 -0400

    Modified interface updater so that it really is just an interface updater

--- include/egocircle_utils/interface_updater.h
+++ include/egocircle_utils/interface_updater.h
@@ -1,5 +1,5 @@
-#ifndef EGO_CIRCLE_COST_WRAPPER_H
-#define EGO_CIRCLE_COST_WRAPPER_H
+#ifndef EGOCIRCLE_UTILS_INTERFACE_UPDATER_H
+#define EGOCIRCLE_UTILS_INTERFACE_UPDATER_H
 
 #include <pips_trajectory_testing/pips_cc_wrapper.h>
 #include <message_filters/subscriber.h>
@@ -8,54 +8,46 @@
 namespace egocircle_utils
 {
 
-class InterfaceUpdater : public pips_trajectory_testing::PipsCCWrapper
-{
-
-  
-private:
-    
-  typedef tf2_ros::MessageFilter<sensor_msgs::LaserScan> TF_Filter;
-  std::shared_ptr<TF_Filter> scan_tf_filter_;
-    
-  message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-
-  sensor_msgs::LaserScan::ConstPtr current_scan_;
-  
-  
-  static constexpr const int MAGIC_NUMBER = -17;  //The sole purpose of this is to ensure that the constructor with the 'optional' tfbuffer argument is not accidentally passed a tfbuffer object
-  
-  std::shared_ptr<UpdateableInterface> cc_;
-    
-  void scanCb(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
-  
-  
-  
-public:
-  // Use this constructor if you don't have a tfbuffer
-  InterfaceUpdater(std::shared_ptr<UpdateableInterface> interface, ros::NodeHandle& nh, ros::NodeHandle& pnh, const std::string& name=DEFAULT_NAME, int tamper_prevention = MAGIC_NUMBER, std::shared_ptr<tf2_ros::Buffer> tf_buffer=std::make_shared<tf2_ros::Buffer>());
-  
-  // Use this constructor if you already have a tfbuffer
-  InterfaceUpdater(std::shared_ptr<UpdateableInterface> interface, ros::NodeHandle& nh, ros::NodeHandle& pnh, std::shared_ptr<tf2_ros::Buffer>& tf_buffer, const std::string& name=DEFAULT_NAME);
-  
-  
-  bool init();
-  
-  void update();
-  
-  bool isReadyImpl();
-  
-  static constexpr const char* DEFAULT_NAME="egocircle_interface_updater";
-  
-  std_msgs::Header getCurrentHeader();
-  
-  std::shared_ptr<pips::collision_testing::TransformingCollisionChecker> getCC()
+  class InterfaceUpdater
   {
-    return cc_;
-  }
 
-};
+  private:
+    ros::NodeHandle nh_, pnh_;
+    tf2_utils::TransformManager tfm_;
+    std::string name_;
+    
+    typedef tf2_ros::MessageFilter<sensor_msgs::LaserScan> TF_Filter;
+    std::shared_ptr<TF_Filter> scan_tf_filter_;
+      
+    message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
+    sensor_msgs::LaserScan::ConstPtr current_scan_;
+      
+    std::shared_ptr<UpdateableInterface> cc_;
+  
+  protected:
+    virtual void scanCb(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
+
+  public:
+    // Use this constructor if you don't have a TransformManager
+    InterfaceUpdater(ros::NodeHandle& nh, ros::NodeHandle& pnh, const std::string& name=DEFAULT_NAME, const tf2_utils::TransformManager& tfm=tf2_utils::TransformManager(false));
+    
+    // Use this constructor if you already have a TransformManager
+    InterfaceUpdater(ros::NodeHandle& nh, ros::NodeHandle& pnh, const tf2_utils::TransformManager& tfm, const std::string& name=DEFAULT_NAME);
+    
+    void setInterface(std::shared_ptr<UpdateableInterface> interface);
+    
+    bool init();
+    
+    void update();
+    
+    bool isReady();
+    
+    static constexpr const char* DEFAULT_NAME="egocircle_interface_updater";
+    
+    std_msgs::Header getCurrentHeader();
+
+  };
 
 }
 
-#endif //EGO_CIRCLE_COST_WRAPPER_H
+#endif //EGOCIRCLE_UTILS_INTERFACE_UPDATER_H
--- include/egocircle_utils/updateable_interface.h
+++ include/egocircle_utils/updateable_interface.h
@@ -1,33 +1,16 @@
 #ifndef EGOCIRCLE_UTILS_UPDATEABLE_INTERFACE_H
 #define EGOCIRCLE_UTILS_UPDATEABLE_INTERFACE_H
 
-#include <pips/collision_testing/transforming_collision_checker.h>
-#include <egocircle_utils/transformer.h>
 #include <sensor_msgs/LaserScan.h>
 
 namespace egocircle_utils
 {
+  class UpdateableInterface
+  {
+    public:
+      virtual void update(const sensor_msgs::LaserScan::ConstPtr& scan)=0;
+  };
 
-class UpdateableInterface : public pips::collision_testing::TransformingCollisionChecker, public egocircle_utils::Transformer
-{
-  public:
-    UpdateableInterface(ros::NodeHandle& nh, ros::NodeHandle& pnh, const std::string& name) :
-      pips::collision_testing::TransformingCollisionChecker(nh,pnh,name)
-      {}
-    
-    virtual void update(const sensor_msgs::LaserScan::ConstPtr& scan)=0;
-    
-    void setTransform(const geometry_msgs::TransformStamped& transform)
-    {
-      Transformer::setTransform(transform);
-    }
-    
-    
-private:
-  //egocircle_utils::Transformer transformer_;
-  
-};
-
-} //namespace
+} // end namespace egocircle_utils
 
 #endif //EGOCIRCLE_UTILS_UPDATEABLE_INTERFACE_H
